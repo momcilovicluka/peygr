46606e18ddf30645acba7ca6067d7718520db969
Luka <l.momcilovic61@gmail.com>
2022-07-18 21:39:24 +0200

    Set up initial structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import Cookies from 'universal-cookie'
+
+import { ChannelListContainer, ChannelContainer } from './components'
+
+import './AppCustom.css';
+
+const apiKey = 'uzypgu2ebb8c';
+
+const client = StreamChat.getInstance(apiKey);
 
 const App = () => {
   return (
-    <div>
-        <h1>Peygr - a pager like chat app</h1>
+    <div className="app__wrapper">
+      <Chat client={client} theme="team light">
+        <ChannelListContainer 
+
+        />
+        <ChannelContainer
+        
+        />
+      </Chat>
     </div>
   )
 }
